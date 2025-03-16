a163d97ec2dea0ac877ce2e2de6517e5044b3e1b
Kavan Kamangar <kavankamangar79@gmail.com>
2025-03-17 00:32:34 +0330

    Added the KYC package

--- Package.swift
+++ Package.swift
@@ -10,12 +10,29 @@ let package = Package(
         .library(
             name: "CreateCustomerSDK",
             targets: ["CreateCustomerSDK"]
-        ),
+        )
+    ],
+    dependencies: [
+        // Add dependency on FPKYCSDK from GitHub.
+        .package(url: "https://github.com/HSMDeveloper/FPKYCSDK.git", from: "1.0.0")
     ],
     targets: [
+        // Your binary target that wraps your compiled framework.
         .binaryTarget(
-            name: "CreateCustomerSDK",
+            name: "CreateCustomerSDKBinary",
             path: "./Frameworks/CreateCustomerSDK.xcframework"
+        ),
+        // Your main target now depends on both your binary and on the FPKYCSDK product.
+        .target(
+            name: "CreateCustomerSDK",
+            dependencies: [
+                "CreateCustomerSDKBinary",
+                .product(name: "FPKYCSDK", package: "FPKYCSDK")
+            ],
+            path: "Sources/CreateCustomerSDKPackage" // specify your custom folder path here
         )
     ]
 )
+
+
+
